fab6e845d3aa9b3677e1c9af83ba5f6aaefca1b8
David Zhu <hdavidzhu@gmail.com>
2018-01-27 13:30:35 -0800

    Add tmux to symlink.

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 BASEDIR=`dirname $0`
-echo $BASEDIR
 
 # Set up symbolink links to the correct documents
 ln -s $BASEDIR/.zshrc ~/.zshrc
 ln -s $BASEDIR/.gitignore_global ~/.gitignore_global
 ln -s $BASEDIR/.vimrc ~/.vimrc
+ln -s $BASEDIR/.tmux.conf ~/.tmux.conf
 
 # Install Vundle
 git clone --quiet https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +qall < /dev/tty # necessary to avoid vim: Input not from terminal warning
+vim +BundleInstall +qall < /dev/tty
