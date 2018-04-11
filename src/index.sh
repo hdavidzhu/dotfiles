d786988ebccec3192adaefd3744b1c522c19d1e4
David Zhu <hdavidzhu@gmail.com>
2018-04-11 11:27:39 -0700

    Use basedir.

--- src/index.sh
+++ src/index.sh
@@ -1,6 +1,8 @@
-source antigen.sh
-source core.sh
-source git.sh
-source lever.sh
-source node.sh
-source nvim.sh
+BASE_DIR=`dirname $0`
+
+source $BASE_DIR/antigen.sh
+source $BASE_DIR/core.sh
+source $BASE_DIR/git.sh
+source $BASE_DIR/lever.sh
+source $BASE_DIR/node.sh
+source $BASE_DIR/nvim.sh
