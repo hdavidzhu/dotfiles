2b7cb574c25da09ecf9438d238fe395f8e7cf2b7
David Zhu <hdavidzhu@gmail.com>
2019-04-19 13:03:36 -0700

    Use nln to get all symlinked packages

--- src/node.sh
+++ src/node.sh
@@ -1,4 +1,3 @@
 alias niu='npm install && npm update'
 alias niu0='niu --depth 0'
-alias nlinks='\ls -F node_modules | sed -n 's/@$//p' | xargs npm ls -g --depth 0'
-alias zmocha='./node_modules/.bin/mocha'
+alias nln='ls -l node_modules | grep ^l'
