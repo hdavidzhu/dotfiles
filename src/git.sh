4541ec0baf049c95172092583c6070c1bf15c879
David Zhu <hdavidzhu@gmail.com>
2020-07-08 16:47:41 +0000

    Git stash clean branch

--- src/git.sh
+++ src/git.sh
@@ -60,3 +60,5 @@ alias gconf='git diff --name-only --diff-filter=U'
 alias gcl='git remote prune origin'
 gitbranchclear() { git branch --merged | grep -v '\*' | xargs -n 1 git branch -d }
 alias gclb='gcl && gitbranchclear && gcl'
+alias gscb='git checkout -q master && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base master $branch) && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
+
