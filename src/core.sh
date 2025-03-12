e68dd4328bac824b372060b282a95d5334d99770
David Zhu <david@vanta.com>
2025-03-12 15:19:15 -0500

    feat: pressing up goes through history

--- src/core.sh
+++ src/core.sh
@@ -1,12 +1,17 @@
 bindkey -v
 
+# Pressing up would search the history for the current command.
+autoload -U up-line-or-beginning-search
+zle -N up-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+
 alias ccc='clear'
 alias editor="vim"
 alias sof='xset dpms force off' # Turn off screen (on linux).
 alias ss='source ~/.zshrc'
 
 # Allows for Ctrl-Z to toggle background processes.
-fancy-ctrl-z () {
+fancy-ctrl-z() {
   if [[ $#BUFFER -eq 0 ]]; then
     BUFFER="fg"
     zle accept-line
